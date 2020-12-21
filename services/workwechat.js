cad3a802407771b8bd2f230264acacc7a8c41033
oujunpeng-xbadge <oujunpeng@xbadge.cn>
2020-12-21 17:47:15 +0800

    add work wechat services

--- services/workwechat.js
+++ services/workwechat.js
@@ -1,5 +1,29 @@
-function getAccessToken(corpid,corpsecret){
-    
+import {kv} from "storage/KVstorage"
 
+function getAccessToken(corpid, corpsecret) {
+    let url = `https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${corpid}&corpsecret=${corpsecret}`
+    fetch(url).then(response => {
+        var resp_json = await response.json()
+        if (resp_json.errcode) {
+            return 0
+        }
+        return resp_json.access_token
+    })
 
 }
+
+
+function postMessage(accesstoken, postData) {
+    let url = `https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${accesstoken}`
+    fetch(url, { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(postData) })
+    .then(response=>{
+      return response
+    })
+
+}
+
+function storeAccessToken(corpid,accesstoken){
+   return await kv.put(corpid,accesstoken,{expirationTtl:7200})
+
+}
+
