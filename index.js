a618c5c87313d7e5203977449714cbf456a6faf5
oujunpeng-xbadge <oujunpeng@xbadge.cn>
2020-12-24 17:13:13 +0800

    for 1.0.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Router = require('./router')
-// import { getAccessToken,postMessage} from "services/workwechat"
+import { getAccessToken, postMessage } from "./services/workwechat"
 /**
  * Example of how router can be used in an application
  *  */
@@ -19,7 +19,7 @@ async function handleRequest(request) {
     const r = new Router()
     // Replace with the appropriate paths and handlers
     // r.get('.*/bar', () => new Response('responding for /bar'))
-    r.get('/foo', request => workerWechatHandler(request))
+    r.post('/message', request => workerWechatHandler(request))
     // r.post('.*/foo.*', request => handler(request))
     // r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
 
@@ -30,16 +30,24 @@ async function handleRequest(request) {
 
 
 
-async function workerWechatHandler(request){
-    let token =  request.headers.get("token") != null? request.headers.get("token"):new URL(request.url).searchParams.get("token")
-    if(!token){
-        return new Response("missing token")
+async function workerWechatHandler(request) {
+    let token = request.headers.get("token") != null ? request.headers.get("token") : new URL(request.url).searchParams.get("token")
+    if (!token) {
+        return new Response("missing key")
     }
-    let text = await WORKWECHAT.get("test")
-    return new Response(text)
-    // let req_json = await request.text()
-    
-
-
-
+    try {
+        let req_json = await request.json()
+        let agentid = req_json.agentid
+        const corpsecret = await WORKWECHAT.get(agentid + '_agentid')
+        let access_token = await WORKWECHAT.get(agentid + '_accessToken')
+        if (access_token === null) {
+            access_token = await getAccessToken(corpsecret)
+            await WORKWECHAT.put(agentid + "_accessToken", access_token, { expirationTtl: 7000 })
+        }
+        const result = await postMessage(access_token, JSON.stringify(req_json))
+        return result
+    }
+    catch (err) {
+         return new Response(err)
+    }
 }
--- services/workwechat.js
+++ services/workwechat.js
@@ -1,23 +1,16 @@
 
-function getAccessToken(corpid, corpsecret) {
-    let url = `https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${corpid}&corpsecret=${corpsecret}`
-    fetch(url).then(async response => {
-        const resp_json = await response.json();
-        if (resp_json.errcode) {
-            return 0
-        }
-        return resp_json.access_token
-    })
-
+async function getAccessToken(corpsecret) {
+    const url = `https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${corpid}&corpsecret=${corpsecret}`
+    var response = await fetch(url)
+    const resp_json = await response.json();
+    return resp_json.access_token
 }
 
 
 function postMessage(accesstoken, postData) {
-    let url = `https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${accesstoken}`
-    fetch(url, { method: "POST", headers: { 'Content-Type': 'application/json' }, body: postData })
-    .then(response=>{
-      return response
-    })
+    const url = `https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${accesstoken}`
+    var response = await fetch(url, { method: "POST", headers: { 'Content-Type': 'application/json' }, body: postData })
+    return response
 
 }
 
